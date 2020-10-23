c7445ea15e0edbe4de6954ffa4f4201557f132ca
Paul Vidal <paul.vid@gmail.com>
2020-10-23 10:38:47 -0400

    Fri Oct 23 10:38:47 EDT 2020

--- aws-pre-req/tmp_network/pvi-cod_aws_delete_network.sh
+++ aws-pre-req/tmp_network/pvi-cod_aws_delete_network.sh
@@ -1,12 +1,12 @@
 
     aws ec2 delete-security-group  --group-id null
     aws ec2 delete-security-group  --group-id null
-    aws ec2 delete-subnet  --subnet-id subnet-005d3661322808a70
-    aws ec2 delete-subnet  --subnet-id subnet-0c7c9b50af8f318e9
-    aws ec2 delete-subnet  --subnet-id subnet-08aef5846a62f9ba6
-    aws ec2 detach-internet-gateway  --internet-gateway-id igw-0d5ed2c641ad64a0a --vpc-id vpc-0b54a490346be37ae
-    aws ec2 delete-route-table  --route-table-id rtb-018378beac3f6bb24
-    aws ec2 delete-vpc  --vpc-id vpc-0b54a490346be37ae
-    aws ec2 delete-internet-gateway  --internet-gateway-id igw-0d5ed2c641ad64a0a
+    aws ec2 delete-subnet  --subnet-id subnet-036d67843a6991978
+    aws ec2 delete-subnet  --subnet-id subnet-03f3a705d8cd24040
+    aws ec2 delete-subnet  --subnet-id subnet-0e8fe3fd42821ffc2
+    aws ec2 detach-internet-gateway  --internet-gateway-id igw-02b08e9ad45f8c140 --vpc-id vpc-00f484a5b8afa3d4a
+    aws ec2 delete-route-table  --route-table-id rtb-054b9d63bb2c2f1b7
+    aws ec2 delete-vpc  --vpc-id vpc-00f484a5b8afa3d4a
+    aws ec2 delete-internet-gateway  --internet-gateway-id igw-02b08e9ad45f8c140
 
     
--- aws-pre-req/tmp_network/qaq_aws_delete_network.sh
+++ aws-pre-req/tmp_network/qaq_aws_delete_network.sh
@@ -1,12 +1,12 @@
 
-    aws ec2 delete-security-group  --group-id 
-    aws ec2 delete-security-group  --group-id 
-    aws ec2 delete-subnet  --subnet-id 
-    aws ec2 delete-subnet  --subnet-id 
-    aws ec2 delete-subnet  --subnet-id 
-    aws ec2 detach-internet-gateway  --internet-gateway-id  --vpc-id 
-    aws ec2 delete-route-table  --route-table-id null
-    aws ec2 delete-vpc  --vpc-id 
-    aws ec2 delete-internet-gateway  --internet-gateway-id 
+    aws ec2 delete-security-group  --group-id null
+    aws ec2 delete-security-group  --group-id null
+    aws ec2 delete-subnet  --subnet-id subnet-061db1ac5022bca27
+    aws ec2 delete-subnet  --subnet-id subnet-069b206af32d57a05
+    aws ec2 delete-subnet  --subnet-id subnet-098579ddac6356b8f
+    aws ec2 detach-internet-gateway  --internet-gateway-id igw-0d3715560263508d3 --vpc-id vpc-081349a344e0b1c8c
+    aws ec2 delete-route-table  --route-table-id rtb-065e97f95f77e822b
+    aws ec2 delete-vpc  --vpc-id vpc-081349a344e0b1c8c
+    aws ec2 delete-internet-gateway  --internet-gateway-id igw-0d3715560263508d3
 
     
